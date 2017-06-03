4a2bc51cfb8573a5a5fe7660dbfe442075952488
yejunyou <ye_junyou@163.com>
2017-06-03 23:32:47 +0800

    添加音乐资源

---
+++ 仿QQ音乐/YYPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYPlayingViewController.h
+//  仿QQ音乐
+//
+//  Created by 叶俊有 on 2017/6/3.
+//  Copyright © 2017年 yejunyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYPlayingViewController : UIViewController
+
+@end
