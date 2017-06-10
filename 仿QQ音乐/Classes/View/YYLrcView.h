b8c68ab12bae6f83b0021e145bac74555ff724c9
yejunyou <ye_junyou@163.com>
2017-06-10 10:39:13 +0800

    add pod files  masonry

---
+++ 仿QQ音乐/Classes/View/YYLrcView.h
@@ -0,0 +1,14 @@
+//
+//  YYLrcView.h
+//  仿QQ音乐
+//
+//  Created by 叶俊有 on 2017/6/8.
+//  Copyright © 2017年 yejunyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYLrcView : UIScrollView
+
+
+@end
---
+++ 仿QQ音乐/Classes/View/YYLrcView.m
@@ -0,0 +1,92 @@
+//
+//  YYLrcView.m
+//  仿QQ音乐
+//
+//  Created by 叶俊有 on 2017/6/8.
+//  Copyright © 2017年 yejunyou. All rights reserved.
+//
+
+#import "YYLrcView.h"
+#import "Masonry.h"
+
+@interface YYLrcView ()<UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation YYLrcView
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    [self addSubview:self.tableView];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self addSubview:self.tableView];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addSubview:self.tableView];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top);
+        make.left.equalTo(self.mas_left).offset(self.bounds.size.width);
+        make.bottom.equalTo(self.mas_bottom);
+        make.right.equalTo(self.mas_right);
+        make.height.equalTo(self.mas_height);
+        make.width.equalTo(self.mas_width);
+    }];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.backgroundColor = [UIColor clearColor];
+        cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"aoghaogh%zd",indexPath.row];
+    return cell;
+}
+
+#pragma mark - setting and getting
+- (UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.backgroundColor = [UIColor clearColor];
+        _tableView.dataSource = self;
+        _tableView.translatesAutoresizingMaskIntoConstraints = NO;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _tableView.rowHeight = 35;
+    }
+    return _tableView;
+}
+@end
